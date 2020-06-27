d9116da4f850017b39555a6d24fc6b32488dabde
Carlton627 <carlton_rodrigues21@outlook.com>
2020-06-27 15:48:45 +0530

    Angle down icon added to banner

--- src/app/layout/header/banner/banner.component.ts
+++ src/app/layout/header/banner/banner.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// font awesome
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-
+  faAngleDown = faAngleDown;
   constructor() { }
 
   ngOnInit(): void {
