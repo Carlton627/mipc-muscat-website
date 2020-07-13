339e05a8dba453baebf4cfb5fa8a8ccc2d9ce282
Carlton627 <carlton_rodrigues21@outlook.com>
2020-07-13 16:55:23 +0530

    Send message implementation started

--- src/app/components/send-message-form/send-message-form.component.ts
+++ src/app/components/send-message-form/send-message-form.component.ts
@@ -7,25 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendMessageFormComponent implements OnInit {
 
-  showModalClass = '';
-  animationClass = 'animate__animated';
-  animationToggle = 'animate__fadeInDown';
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  private resetAnimationValues() {
-    this.animationToggle = 'animate__fadeInDown';
-    this.showModalClass = '';
-  }
-
-  toggleModal = () => {
-    this.resetAnimationValues();
-    this.showModalClass = 'show';
-  }
-
-  toggleEntryAndExit = () => {
-    this.animationToggle = 'animate__fadeOutUp';
   }
 }
