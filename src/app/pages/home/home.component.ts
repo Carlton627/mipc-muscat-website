05f6b08383ab7c8ea1fdf10f9e1f1d4434c75f50
Carlton627 <carlton_rodrigues21@outlook.com>
2020-07-25 12:17:27 +0530

    Core team section: Scrollable cards implemented

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { cardData } from 'src/utils/feature_card_data';
+import { interval } from 'rxjs';
+import { takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cardData = cardData;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  // core team section [start]
+  scrollCardsHorizontallyLeft = (el: Element) => {
+    const animTimeMs = 350;
+    const pixelsToMove = 315;
+    const stepArray = [0.001, 0.021, 0.136, 0.341, 0.341, 0.136, 0.021, 0.001];
+    interval(animTimeMs / 8)
+      .pipe(
+        takeWhile(value => value < 8),
+        tap(value => el.scrollLeft -= (pixelsToMove * stepArray[value])),
+      )
+      .subscribe();
+  }
+
+  scrollCardsHorizontallyRight = (el: Element) => {
+    const animTimeMs = 350;
+    const pixelsToMove = 300;
+    const stepArray = [0.001, 0.021, 0.136, 0.341, 0.341, 0.136, 0.021, 0.001];
+    interval(animTimeMs / 10)
+      .pipe(
+        takeWhile(value => value < 8),
+        tap(value => el.scrollLeft += (pixelsToMove * stepArray[value])),
+      )
+      .subscribe();
+  }
+
+  showLeftButton(el: Element) {
+    if (el.scrollLeft === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  showRightButton(el: Element) {
+    const maxScrollWidth = el.scrollWidth - el.clientWidth;
+    if (Math.ceil(el.scrollLeft) === maxScrollWidth) {
+      return false;
+    }
+    return true;
+  }
+
+  scrollEvent(element, event) {
+    this.showLeftButton(element);
+  }
+  // core team section [end]
 }
---
+++ src/utils/feature_card_data.ts
@@ -0,0 +1,42 @@
+export const cardData = [
+    {
+        cardTitle: 'Blaise Rodrigues',
+        cardDesc: 'Lorem ipsum dolor set amit.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Carlton Rodrigues',
+        cardDesc: 'Dorem ilsum solor dot amet.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Shawn Rodrigues',
+        cardDesc: 'Sorem ilpum solri det aret.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Undefined',
+        cardDesc: 'Undefined Undefined Lorem ipsum.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Undefined',
+        cardDesc: 'Undefined Undefined Lorem ipsum.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Undefined',
+        cardDesc: 'Undefined Undefined Lorem ipsum.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Undefined',
+        cardDesc: 'Undefined Undefined Lorem ipsum.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+    {
+        cardTitle: 'Undefined',
+        cardDesc: 'Undefined Undefined Lorem ipsum.',
+        imgSrc: '../../../assets/mipc_logo.png',
+    },
+];
